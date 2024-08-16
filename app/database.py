4fb4bec1a7874a625d77271f5f588250ca68b823
Reshie <reo040125@gmail.com>
2024-08-16 07:32:26 +0000

    Update database connection URL in database.py and conftest.py

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('postgresql://postgres:postgres@localhost:5432/postgres')
+engine = create_engine('postgresql://postgres:postgres@docker_workspace_devcontainer-db-1:5432/postgres')
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
--- app/test/conftest.py
+++ app/test/conftest.py
@@ -1,15 +1,18 @@
 import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy_utils import database_exists, drop_database
+from sqlalchemy_utils import create_database, database_exists, drop_database
 from app.database import Base
 
 @pytest.fixture(scope="function")
 def SessionLocal():
-    TEST_SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/postgres_temp"
+    TEST_SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@docker_workspace_devcontainer-db-1:5432/postgres_temp"
+    
     engine = create_engine(TEST_SQLALCHEMY_DATABASE_URL)
+    if not database_exists(TEST_SQLALCHEMY_DATABASE_URL):
+        create_database(TEST_SQLALCHEMY_DATABASE_URL)
 
-    assert not database_exists(TEST_SQLALCHEMY_DATABASE_URL), "Test database already exists. Aborting tests."
+    # assert not database_exists(TEST_SQLALCHEMY_DATABASE_URL), "Test database already exists. Aborting tests."
 
     Base.metadata.create_all(engine)
     SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
