ab8c2e67b35afc23c3ca6cfd0a4dc3dabb7af8c6
Reshie <reo040125@gmail.com>
2024-08-19 18:04:14 +0900

    Add CORS middleware for allowing cross-origin requests

--- app/main.py
+++ app/main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from sqlalchemy import create_engine
+from fastapi.middleware.cors import CORSMiddleware
 
-from app import models, routers
+from app import routers
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="./app/static"), name="static")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 app.include_router(routers.router)
