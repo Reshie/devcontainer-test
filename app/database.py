add6498e0acefa477d282182774fad361b3612b6
Reshie <reo040125@gmail.com>
2024-09-01 09:53:54 +0900

    save progress

--- app/database.py
+++ app/database.py
@@ -1,8 +1,36 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
+from google.cloud.sql.connector import Connector
+import pg8000
+from dotenv import load_dotenv
+import os
 
-engine = create_engine('postgresql://postgres:postgres@docker_workspace_devcontainer-db-1:5432/postgres')
+### when using docker-compose ###
+# engine = create_engine('postgresql://postgres:postgres@docker_workspace_devcontainer-db-1:5432/postgres')
+
+load_dotenv()
+
+db_user = os.getenv("DB_USER")
+db_pass = os.getenv("DB_PASS")
+db_name = os.getenv("DB_NAME")
+instance_connection_name = os.getenv("INSTANCE_CONNECTION_NAME")
+
+connector = Connector()
+
+def getconn() -> pg8000.dbapi.Connection:
+    print(db_name)
+    conn: pg8000.dbapi.Connection = connector.connect(
+        instance_connection_name,
+        "pg8000",
+        user=db_user,
+        password=db_pass,
+        db=db_name,
+    )
+    return conn
+
+engine = create_engine("postgresql+pg8000://", creator=getconn)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
--- app/routers.py
+++ app/routers.py
@@ -13,11 +13,11 @@ router = APIRouter()
 models.Base.metadata.create_all(engine)
 
 def get_db():
-	db = SessionLocal()
-	try:
-		yield db
-	finally:
-		db.close()
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @router.get("/", response_class=HTMLResponse)
 async def root(request: Request):
@@ -26,23 +26,23 @@ async def root(request: Request):
 
 @router.get("/task", response_model=List[Task])
 def get_tasks(db: Session = Depends(get_db)):
-	tasks = crud.get_all_tasks(db=db)
-	return tasks
+    tasks = crud.get_all_tasks(db=db)
+    return tasks
 
 @router.post("/task", response_model=Task)
 def create_task(data: TaskCreate, db: Session = Depends(get_db)):
-	task = crud.create_task(db=db, title=data.title)
-	return task
+    task = crud.create_task(db=db, title=data.title)
+    return task
 
 @router.put("/task/{task_id}", response_model=Task)
 def toggle_task(task_id: int, db: Session = Depends(get_db)):
-	task = crud.toggle_task(db=db, task_id=task_id)
-	return task
+    task = crud.toggle_task(db=db, task_id=task_id)
+    return task
 
 @router.delete("/task/{task_id}", response_model=Task)
 def delete_task(task_id: int, db: Session = Depends(get_db)):
-	task = crud.delete_task(db=db, task_id=task_id)
-	return task
+    task = crud.delete_task(db=db, task_id=task_id)
+    return task
 
 @router.get("/search", response_class=HTMLResponse)
 async def root(request: Request):
